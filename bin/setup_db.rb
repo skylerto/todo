c51137a76081774d2d7903ef1e3c09656f2cbd70
Skyler Layne <skylerclayne@gmail.com>
2016-04-04 17:13:02 -0400

    create ruby script to setup database

---
+++ bin/setup_db.rb
@@ -0,0 +1,20 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    :adapter=> "mysql",
+    :host => "localhost",
+    :database=> "todo"
+)
+
+class CreateTodos < ActiveRecord::Migration
+    def self.up
+        create_table :todo do |t|
+        t.string :name, null: false
+        t.boolean :done, null: false, default: false
+    end
+end
+
+CreateTodos.up
+
+end
+
